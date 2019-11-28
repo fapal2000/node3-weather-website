8b25640a1fccb21dac594225e4de18d756248403
Palma Fabrizio <palmaf@corp.dom>
2019-11-28 15:31:04 +0100

    Rimozione di stampe non necessarie su consolle

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log('Console js loaded')
-/*
-fetch('/wheater?address=roma').then((response)=> {
-	response.json().then((data)=> {
-		if(data.error)
-			console.log(data.error)
-		else {
-			console.log(data.location)
-			console.log(data.forecast)
-		}
-	})
-})
-*/
 const wheaterForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -21,7 +8,6 @@ wheaterForm.addEventListener('submit',(e) => {
 	messageOne.textContent='Caricamento......'
 	messageTwo.textContent=''
 	const location = search.value
-	//console.log('<' + location + '>')
 	fetch('/wheater?address='+location).then((response)=> {
 		response.json().then((data)=> {
 			if(data.error) {
