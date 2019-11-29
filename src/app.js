b0fda78363d3984af750a852bec58dc80434911f
Palma Fabrizio <palmaf@corp.dom>
2019-11-29 16:08:13 +0100

    ulteriori info

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.get('',(req,res)=> {
 })
 app.get('/about',(req,res)=> {
 	res.render('about',{
-		title:'About',
+		title:'Pagina di Info',
 		name:'Isabella',
 		footermsg : 'Creata da Fabrizio'
 	})
